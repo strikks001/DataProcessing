57e581a539bb8a634531f67f3348f59befb659ae
Sanne Strikkers <sanne.strikkers@hva.nl>
2016-05-06 12:18:42 +0200

    part 2 almost done. Only the questions are left.

--- Homework/week-2 : week-3/D3/d3map/d3map.js
+++ Homework/week-2 : week-3/D3/d3map/d3map.js
@@ -1,19 +1,68 @@
-d3.csv("population.csv", function (error, data) {
-    if (error) throw error;
+/**
 
-    var population = []
-    data.forEach(function (d) {
-        population = [d.Code, d.Country, d.Population];
-    });
-    
+d3map.js
+
+Computer Science
+Data Processing Part 2
+
+JavaScript D3 Datamap to create a map and fill it with colors
+
+Sanne Strikkers
+11170816
+
+*/
+
+// setting up the map
 var map = new Datamap({
-        element: document.getElementById('container1'),
-        scope: 'world',
-        fills: {
-            defaultFill: '#B8B8B8'
-        },
-        data: {
-   
+    element: document.getElementById('container1'),
+    scope: 'world',
+    fills: {
+        defaultFill: '#B8B8B8',
+        h10: '#67000d',
+        h9: '#a50f15',
+        h8: '#cb181d',
+        m7: '#ef3b2c',
+        m6: '#fb6a4a',
+        m5: '#fc9272',
+        l4: '#fcbba1',
+        l3: '#fee0d2',
+        l2: '#fff5f0'
+    },
+    geographyConfig: {
+        borderColor: '#FAFAFA',
+        borderWidth: 1,
+        highlightBorderWidth: 1.5,
+        highlightBorderColor: '#D8D8D8',
+        highlightFillColor: '#C41E4A',
+        popupTemplate: function (geo, data) {
+            return ['<div class="popup-tool">',
+                    'Country: <strong>' + geo.properties.name,
+                    '</strong><br>Population (x 1.000): <strong>' + data.population,
+                    '</strong></div>'].join('');
         }
+    }
+});
+
+// legenda for the map
+map.legend({
+    legendTitle: "World Population x 1.000",
+    labels: {
+        h10: '1.000.000 >',
+        h9: '200.000 - 1.000.000',
+        h8: '100.000 - 200.000',
+        m7: '75.000 - 100.000',
+        m6: '50.000 - 75.000',
+        m5: '2.500 - 50.000',
+        l4: '1.000 - 2.500',
+        l3: '500 - 1.000',
+        l2: '< 500'
+    }
+});
+
+// get data from json file
+d3.json("data.json", function (error, data) {
+    if (error) throw error;
+    data.forEach(function (d) {
+        map.updateChoropleth(d);
     });
 });
--- Homework/week-2 : week-3/D3/d3map/script.py
+++ Homework/week-2 : week-3/D3/d3map/script.py
@@ -19,17 +19,39 @@ with open(csvfile, 'r') as f:
         mydict = list(reader)
     except csv.Error as e:
         sys.exit('file %s, line %d: %s' % (f, reader.line_num, e))
-
         
 # format the dictionary to a list of key-value pairs
 data_list = []
 for index in range(len(mydict)):
-    data_list.append({"code": mydict[index][1], "country": mydict[index][0], "population": mydict[index][2]})
+    # split information into variables
+    population = int(mydict[index][2])
+    country = mydict[index][0]
+    code = mydict[index][1]
+    
+    # give values a different fillKey
+    if (population >= 1000000):
+        data_list.append({code: {"fillKey": "h10", "country": country, "population": population}})
+    elif (population >= 200000 and population < 1000000):
+        data_list.append({code: {"fillKey": "h9", "country": country, "population": population}})
+    elif (population >= 100000 and population < 200000):
+        data_list.append({code: {"fillKey": "h8", "country": country, "population": population}})
+    elif (population >= 75000 and population < 100000):
+        data_list.append({code: {"fillKey": "m7", "country": country, "population": population}})
+    elif (population >= 50000 and population < 75000):
+        data_list.append({code: {"fillKey": "m6", "country": country, "population": population}})
+    elif (population >= 2500 and population < 50000):
+        data_list.append({code: {"fillKey": "m5", "country": country, "population": population}})
+    elif (population >= 1000 and population < 2500):
+        data_list.append({code: {"fillKey": "l4", "country": country, "population": population}})
+    elif (population >= 500 and population < 1000):
+        data_list.append({code: {"fillKey": "l3", "country": country, "population": population}})
+    elif (population < 500):
+        data_list.append({code: {"fillKey": "l2", "country": country, "population": population}})
 
-# print data_list
-   
+
+        
 # convert the list to JSON format
-json_str = json.dumps(data_list, ensure_ascii=False)
+json_str = json.dumps(data_list, sort_keys=True, indent=4, ensure_ascii=False)
 
 jsonfile = 'data.json'
 with open(jsonfile, 'w') as f:
@@ -37,3 +59,4 @@ with open(jsonfile, 'w') as f:
         f.write(json_str)
     except csv.Error as e:
         sys.exit('file %s, line %d: %s' % (f, reader.line_num, e))
+
