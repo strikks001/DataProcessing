1f3c41fe8dacb4a30c2bc00a46517fb98f8f8981
Sanne Strikkers <sanne.strikkers@hva.nl>
2016-05-20 17:51:09 +0200

    Functionality is done! TODO: cleaning up the code, adding a legend for the world map, adding some explanation, making it more understandable

--- Homework/week-5/D3LinkedViews/D3LinkedViews.js
+++ Homework/week-5/D3LinkedViews/D3LinkedViews.js
@@ -27,213 +27,339 @@ var map = new Datamap({
         l4: '#fcbba1',
         l3: '#fee0d2',
         l2: '#fff5f0',
-        no: "#B8B8B8"
+        no: "#B8B8B8",
+        wb_good: "#91cf60",
+        wb_middle: "#ffffd4",
+        wb_poor: "#fc8d59",
+        ef_good: "#1a9641",
+        ef_middle: "#a6d96a",
+        ef_poor: "#fdae61",
+        ef_deep_red: "#d7191c",
+        lf_good: "#91cf60",
+        lf_middle: "#ffffd4",
+        lf_poor: "#fc8d59",
     },
     geographyConfig: {
-        borderColor: '#FAFAFA',
-        borderWidth: 1,
-        highlightBorderWidth: 1.5,
+        borderColor: '#4d4d4d',
+        borderWidth: 0.5,
+        highlightBorderWidth: 2.5,
         highlightBorderColor: '#D8D8D8',
-        highlightFillColor: '#C41E4A',
+        highlightFillColor: '#525252',
         popupTemplate: function (geo, data) {
             return ['<div class="popup-tool">',
                     'Country: <strong>' + geo.properties.name,
                     '</strong><br>Population: <strong>' + data.population,
                     '</strong></div>'].join('');
         }
+    },
+    done: function (datamap) {
+        datamap.svg.selectAll('.datamaps-subunit').on('click', function (geography) {
+            //alert(geography.properties.name);
+            getData(geography.id);
+        });
     }
 });
 
-// legenda for the map
-map.legend({
-    legendTitle: "World Population in 2010",
-    labels: {
-        h10: '1.000.000.000 >',
-        h9: '200.000.000 - 1.000.000.000',
-        h8: '100.000.000 - 200.000.000',
-        m7: '75.000.000 - 100.000.000',
-        m6: '50.000.000 - 75.000.000',
-        m5: '2.500.000 - 50.000.000',
-        l4: '1.000.000 - 2.500.000',
-        l3: '500.000 - 1.000.000',
-        l2: '< 500.000',
-        no: "No data available"
+updateMap("World Population");
+
+function getData(countryAdd) {
+    // get data from json file and update the map with these values
+    d3.json("data/D3LinkedViews.json", function (error, data) {
+        if (error) {
+            console.log("We cannot retrieve the data.");
+            alert("We cannot retrieve the data.");
+            throw error;
+        };
+
+        var china, netherlands, usa, countryNew = {};
+        var countries = [];
+
+        for (i in map.options.data) {
+            if (map.options.data[i].country == "China") {
+                china = map.options.data[i];
+            } else if (map.options.data[i].country == "Netherlands") {
+                netherlands = map.options.data[i];
+            } else if (map.options.data[i].country == "United States") {
+                usa = map.options.data[i];
+            }
+
+            if (i = countryAdd) {
+                countryNew = map.options.data[i];
+            }
+        }
+        
+        countries.push(china, netherlands, usa, countryNew);
+
+        //        d3.keys(map.options.data).forEach(function (key) {
+        //            if (key = countryAdd){
+        //                    countryNew = map.options.data[key];
+        //            }     
+        //        });
+
+        createBarchart(createData("Ecological Footprint", countries), "chart1");
+        createBarchart(createData("Well-being", countries), "chart2");
+        createBarchart(createData("Life Expectancy", countries), "chart3");
+
+        d3.select("#title-hpi")
+            .text("Happy Life Index");
+
+        d3.select("#under-hpi")
+            .text(countryNew.country + ": " + countryNew.hpi);
+
+    });
+}
+
+function createBarchart(data1, selection) {
+    // sizing the bar chart
+    var chartWidth = 300,
+        barHeight = 20,
+        groupHeight = barHeight * data1.series.length,
+        gapBetweenGroups = 10,
+        spaceForLabels = 150,
+        spaceForLegend = 150;
+
+    // Zip the series data together (first values, second values, etc.)
+    var zippedData = [];
+    for (var i = 0; i < data1.labels.length; i++) {
+        for (var j = 0; j < data1.series.length; j++) {
+            zippedData.push(data1.series[j].values[i]);
+        }
     }
-});
 
-// get data from json file and update the map with these values
-d3.json("data/D3LinkedViews.json", function (error, data) {
-    if (error) {
-        console.log("We cannot retrieve the data.");
-        alert("We cannot retrieve the data.");
-        throw error;
-    };
+    // set up the colors for several lines
+    var color = d3.scale.ordinal().range(["#8dd3c7", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd"]);
 
-    data.forEach(function (d) {
-        map.updateChoropleth(d);
+    var chartHeight = barHeight * zippedData.length + gapBetweenGroups * data1.labels.length;
+
+    // x-axis
+    var x = d3.scale.linear()
+        .domain([0, d3.max(zippedData)])
+        .range([0, chartWidth]);
+
+    // y -axis
+    var y = d3.scale.linear()
+        .range([chartHeight + gapBetweenGroups, 0]);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickFormat('')
+        .tickSize(0)
+        .orient("left");
+
+    // Specify the chart area and dimensions
+    var chart = d3.select("." + selection)
+        .attr("width", spaceForLabels + chartWidth + spaceForLegend)
+        .attr("height", chartHeight);
+
+    // Create bars
+    var bar = chart.selectAll("g")
+        .data(zippedData);
+
+    var barEnter = bar
+        .enter().append("g")
+        .attr("transform", function (d, i) {
+            return "translate(" + spaceForLabels + "," + (i * barHeight + gapBetweenGroups * (0.5 + Math.floor(i / data1.series.length))) + ")";
+        });
+
+    // Create rectangles of the correct width
+    barEnter.append("rect")
+        .attr("fill", function (d, i) {
+            return color(i % data1.series.length);
+        })
+        .attr("class", "bar")
+        .attr("width", x)
+        .attr("height", barHeight - 1);
+
+    // Add text label in bar
+    barEnter.append("text")
+        .attr("x", function (d) {
+            return x(d) - 3;
+        })
+        .attr("y", barHeight / 2)
+        .attr("fill", "red")
+        .attr("dy", ".35em")
+        .text(function (d) {
+            return d;
+        });
+
+    // Draw labels
+    barEnter.append("text")
+        .attr("class", "label")
+        .attr("x", function (d) {
+            return -10;
+        })
+        .attr("y", groupHeight / 2)
+        .attr("dy", ".35em")
+        .text(function (d, i) {
+            if (i % data1.series.length === 0)
+                return data1.labels[Math.floor(i / data1.series.length)];
+            else
+                return ""
+        });
+
+    chart.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + spaceForLabels + ", " + -gapBetweenGroups / 2 + ")")
+        .call(yAxis);
+
+    // UPDATE PART
+    barUpdate = d3.transition(bar);
+
+    barUpdate.select('rect')
+        .transition().duration(600)
+        .attr("fill", function (d, i) {
+            return color(i % data1.series.length);
+        })
+        .attr("width", x)
+        .attr("height", barHeight - 1);
+
+    barUpdate.select('text')
+        .transition().duration(600)
+        .attr("x", function (d) {
+            return x(d) - 3;
+        })
+        .attr("y", barHeight / 2)
+        .attr("fill", "red")
+        .attr("dy", ".35em")
+        .text(function (d) {
+            return d;
+        });
+
+    bar.exit().remove();
+
+    // Draw legend
+    var legendRectSize = 18,
+        legendSpacing = 4;
+
+    var legend = chart.selectAll('.legend')
+        .data(data1.series)
+        .enter()
+        .append('g')
+        .attr('transform', function (d, i) {
+            var height = legendRectSize + legendSpacing;
+            var offset = -gapBetweenGroups / 2;
+            var horz = spaceForLabels + chartWidth + 40 - legendRectSize;
+            var vert = i * height - offset;
+            return 'translate(' + horz + ',' + vert + ')';
+        });
+
+    legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', function (d, i) {
+            return color(i);
+        })
+        .style('stroke', function (d, i) {
+            return color(i);
+        });
+
+    legend.append('text')
+        .attr('class', 'legend')
+        .attr('x', legendRectSize + legendSpacing)
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function (d) {
+            return d.label;
+        });
+
+}
+
+// selecting a city will update the chart 
+d3.selectAll("select")
+    .on("change", function () {
+        // update world map
+        var value = d3.select(this).property('value');
+        updateMap(value);
     });
 
-    for (i in map.options.data) {
-        if (map.options.data[i].population >= 1000000000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "h10";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
-        }
-        if (map.options.data[i].population >= 200000000 && map.options.data[i].population < 1000000000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "h9";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
 
-        } else if (map.options.data[i].population >= 100000000 && map.options.data[i].population < 200000000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "h8";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
+function updateMap(subject) {
+    var jsonFile = "data/D3LinkedViews_pop.json";
 
-        } else if (map.options.data[i].population >= 75000000 && map.options.data[i].population < 100000000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "m7";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
-
-        } else if (map.options.data[i].population >= 50000000 && map.options.data[i].population < 75000000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "m6";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
-
-        } else if (map.options.data[i].population >= 2500000 && map.options.data[i].population < 50000000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "m5";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
-
-        } else if (map.options.data[i].population >= 1000000 && map.options.data[i].population < 2500000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "l4";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
-
-        } else if (map.options.data[i].population >= 500000 && map.options.data[i].population < 1000000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "l3";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
-
-        } else if (map.options.data[i].population < 500000) {
-            var fillKey = {};
-            var data = {};
-            fillKey["fillKey"] = "l2";
-            data[i] = fillKey;
-            map.updateChoropleth(data);
-        }
+    if (subject == "Life Expectancy") {
+        jsonFile = "data/D3LinkedViews_lf.json";
+        d3.select("#map-title")
+            .text(subject + " in 2011");
+    } else if (subject == "Well-being") {
+        jsonFile = "data/D3LinkedViews_wb.json";
+        d3.select("#map-title")
+            .text(subject + " in 2012");
+    } else if (subject == "Ecological Footprint") {
+        jsonFile = "data/D3LinkedViews_ef.json";
+        d3.select("#map-title")
+            .text(subject + " in 2008");
+    } else if (subject == "World Population") {
+        jsonFile = "data/D3LinkedViews_pop.json";
+        d3.select("#map-title")
+            .text(subject + " in 2010");
     }
 
-});
+    // get data from json file and update the map with these values
+    d3.json(jsonFile, function (error, data) {
+        if (error) {
+            console.log("We cannot retrieve the data.");
+            alert("We cannot retrieve the data.");
+            throw error;
+        };
 
-/*
-BarChart
-*/
+        data.forEach(function (d) {
+            map.updateChoropleth(d);
+        });
+    });
+}
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function createData(subject, countries) {
+    var data = {};
 
-var x0 = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
-
-var x1 = d3.scale.ordinal();
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-var xAxis = d3.svg.axis()
-    .scale(x0)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickFormat(d3.format(".2s"));
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.json("data/D3LinkedViews.json", function(error, data) {
-  if (error) throw error;
-
-  var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
-
-  data.forEach(function(d) {
-    d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
-  });
-
-  x0.domain(data.map(function(d) { return d.State; }));
-  x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
-  y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Population");
-
-  var state = svg.selectAll(".state")
-      .data(data)
-    .enter().append("g")
-      .attr("class", "state")
-      .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
-
-  state.selectAll("rect")
-      .data(function(d) { return d.ages; })
-    .enter().append("rect")
-      .attr("width", x1.rangeBand())
-      .attr("x", function(d) { return x1(d.name); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .style("fill", function(d) { return color(d.name); });
-
-  var legend = svg.selectAll(".legend")
-      .data(ageNames.slice().reverse())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
-});
+    if (subject == "Life Expectancy") {
+        data = {
+            labels: [subject],
+            series: [{
+                label: countries[0].country,
+                values: [parseFloat(countries[0].lifeExpectancy)]
+            }, {
+                label: countries[1].country,
+                values: [parseFloat(countries[1].lifeExpectancy)]
+            }, {
+                label: countries[2].country,
+                values: [parseFloat(countries[2].lifeExpectancy)]
+            }, {
+                label: countries[3].country,
+                values: [parseFloat(countries[3].lifeExpectancy)]
+            }]
+        };
+    } else if (subject == "Well-being") {
+        data = {
+            labels: [subject],
+            series: [{
+                label: countries[0].country,
+                values: [parseFloat(countries[0].wellBeing)]
+            }, {
+                label: countries[1].country,
+                values: [parseFloat(countries[1].wellBeing)]
+            }, {
+                label: countries[2].country,
+                values: [parseFloat(countries[2].wellBeing)]
+            }, {
+                label: countries[3].country,
+                values: [parseFloat(countries[3].wellBeing)]
+            }]
+        };
+    } else if (subject == "Ecological Footprint") {
+        data = {
+            labels: [subject],
+            series: [{
+                label: countries[0].country,
+                values: [parseFloat(countries[0].ecologicalFootprint)]
+            }, {
+                label: countries[1].country,
+                values: [parseFloat(countries[1].ecologicalFootprint)]
+            }, {
+                label: countries[2].country,
+                values: [parseFloat(countries[2].ecologicalFootprint)]
+            }, {
+                label: countries[3].country,
+                values: [parseFloat(countries[3].ecologicalFootprint)]
+            }]
+        };
+    }
+    return data;
+}
--- Homework/week-5/D3LinkedViews/data/D3LinkedViews.py
+++ Homework/week-5/D3LinkedViews/data/D3LinkedViews.py
@@ -1,5 +1,16 @@
 import csv, sys, json
 
+def createJson(data_list, sub):
+    # convert the list to JSON format
+    json_str = json.dumps(data_list, indent = 4, ensure_ascii=False)
+
+    jsonfile = 'D3LinkedViews%s.json' %sub
+    with open(jsonfile, 'w') as f:
+        try:
+            f.write(json_str)
+        except csv.Error as e:
+            sys.exit('file %s, line %d: %s' % (f, reader.line_num, e))
+
 # open tsv file and read it into a dictionary
 tsvfile = 'D3LinkedViews.tsv'
 country_codes = 'country_codes.tsv'
@@ -21,16 +32,64 @@ def getCountryCode(country):
             return codedict[i][1]
 
 # format the dictionary to a list of key-value pairs
-data_list = []  
+data_list_population = [] 
+data_list_lifeExpectancy = []
+data_list_wellBeing = [] 
+data_list_ecologicalFootprint = [] 
+data_list_hpi = []
+
 for i in range(len(mydict)):
-    data_list.append({getCountryCode(mydict[i][0]): {"country": mydict[i][0], "population": mydict[i][5], "lifeExpectancy": mydict[i][1], "wellBeing": mydict[i][2], "ecologicalFootprint": mydict[i][3], "hpi": mydict[i][4], "fillKey": ""}})
+    country = mydict[i][0]
+    population = int(mydict[i][5])
+    lifeExpectancy = float(mydict[i][1].replace(',','.'))
+    wellBeing = float(mydict[i][2].replace(',','.'))
+    ecologicalFootprint = float(mydict[i][3].replace(',','.'))
+    hpi = float(mydict[i][4].replace(',','.'))
+   
+    # give values a different fillKey
+    if population >= 1000000000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "h10"}})
+    elif population >= 200000000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "h9"}})
+    elif population >= 100000000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "h8"}})
+    elif population >= 75000000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "m7"}})
+    elif population >= 50000000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "m6"}})
+    elif population >= 2500000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "m5"}})
+    elif population >= 1000000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "l4"}})
+    elif population >= 500000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "l3"}})
+    elif population < 500000:
+        data_list_population.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "l2"}})
+    
+    if lifeExpectancy >= 75:
+        data_list_lifeExpectancy.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "lf_good"}})
+    elif lifeExpectancy >= 60 and lifeExpectancy < 75:
+        data_list_lifeExpectancy.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "lf_middle"}})
+    elif lifeExpectancy < 60:
+        data_list_lifeExpectancy.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "lf_poor"}})
+        
+    if wellBeing >= 6.2:
+        data_list_wellBeing.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "wb_good"}})
+    elif wellBeing >= 4.8 and wellBeing < 6.2:
+        data_list_wellBeing.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "wb_middle"}})
+    elif wellBeing < 4.8:
+        data_list_wellBeing.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "wb_poor"}})
+       
+    if ecologicalFootprint <= 1.78:
+        data_list_ecologicalFootprint.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "ef_good"}})
+    elif ecologicalFootprint > 1.78 and ecologicalFootprint < 3.56:
+        data_list_ecologicalFootprint.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "ef_middle"}})
+    elif ecologicalFootprint >= 3.56 and ecologicalFootprint < 7.12:
+        data_list_ecologicalFootprint.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "ef_poor"}})
+    elif ecologicalFootprint > 7.12:
+        data_list_ecologicalFootprint.append({getCountryCode(mydict[i][0]): {"country": country, "population": population, "lifeExpectancy": lifeExpectancy, "wellBeing": wellBeing, "ecologicalFootprint": ecologicalFootprint, "hpi": hpi, "fillKey": "ef_deep_red"}})
 
-# convert the list to JSON format
-json_str = json.dumps(data_list, indent = 4, ensure_ascii=False)
-
-jsonfile = 'D3LinkedViews.json'
-with open(jsonfile, 'w') as f:
-    try:
-        f.write(json_str)
-    except csv.Error as e:
-        sys.exit('file %s, line %d: %s' % (f, reader.line_num, e))
+createJson(data_list_population, "_pop")
+createJson(data_list_lifeExpectancy, "_lf")
+createJson(data_list_wellBeing, "_wb")
+createJson(data_list_ecologicalFootprint, "_ef")
